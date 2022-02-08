285ab781a87b24e714fe774cbefa56bb90c67cba
Ozymandias <tomas.rav@gmail.com>
2022-02-08 12:23:06 +0200

    - Added preact

--- next.config.js
+++ next.config.js
@@ -3,13 +3,26 @@
 
 const { withExpo } = require("@expo/next-adapter");
 const withPlugins = require("next-compose-plugins");
+const withPreact = require("next-plugin-preact");
 const withRNWeb = require("next-transpile-modules")(["react-native-web"]);
 const withImages = require("next-images");
 const withFonts = require("next-fonts");
 
-
 module.exports = withPlugins([
-	withRNWeb,
+	withPreact,
+	[
+		withRNWeb,
+		{
+			webpack: (config) => {
+				config.resolve.alias = {
+					...config.resolve.alias,
+					"react-dom/unstable-native-dependencies$":
+						"preact-responder-event-plugin",
+				};
+				return config;
+			},
+		},
+	],
 	withImages,
 	withFonts,
 	[
